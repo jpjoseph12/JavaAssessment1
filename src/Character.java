4509239c53dd1d82c5d6f6463cf9990fa588d407
jpjoseph12 <jpjoseph12@gmail.com>
2018-08-24 14:00:59 +0100

    Assessment

--- src/Character.java
+++ src/Character.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public abstract class Character {
+	//was planning to make sub calsses for each character but decided there was no need to!
 	ArrayList<Character>characters = new arrayList<Character>;
 	character.add()
 	public void character(String name, int position) {
